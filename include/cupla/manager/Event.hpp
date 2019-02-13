24d159aa8dc466282d3f9c2705a5b0ee1ba35b4d
Hauke Mewes <haukemewes@googlemail.com>
2019-02-13 14:52:32 +0100

    Added conditional check for backends.

--- include/cupla/manager/Event.hpp
+++ include/cupla/manager/Event.hpp
@@ -61,8 +61,11 @@ namespace detail
             hasTimer( !( flags & cuplaEventDisableTiming ) ),
             event(
                 new AlpakaEvent(
-                    Device< T_DeviceType >::get().current(),
-                    !(flags & cuplaEventBlockingSync)
+                    Device< T_DeviceType >::get().current()
+                    // The alpaka interfaces for this constructor are different depending on the backend.
+#if( ALPAKA_ACC_GPU_HIP_ENABLED == 1 || ALPAKA_ACC_GPU_CUDA_ENABLED == 1 )
+                    ,!(flags & cuplaEventBlockingSync)
+#endif
                 )
             )
         {
