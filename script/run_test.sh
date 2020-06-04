b4f9fc0c3d3c5b2ad44e5af8aba1695018cf48a5
Simeon Ehrig <s.ehrig@hzdr.de>
2020-06-04 15:03:21 +0200

    GitLab-CI: Extended run_test.sh to iterate over different versions
    - iterate over: GCC/Clang versions, Boost version and Alpaka ACC versions

--- script/run_test.sh
+++ script/run_test.sh
@@ -8,30 +8,51 @@ if [[ ! -v CUPLA_BUILD_TYPE ]] ; then
     CUPLA_BUILD_TYPE=Release ;
 fi
 
-echo "number of processor threads $(nproc)"
-$CXX --version
-cmake --version
-echo "Boost version-> $BOOST_VERSION"
-export cupla_DIR=$CI_PROJECT_DIR
+###################################################
+# cmake config builder
+###################################################
+
+CUPLA_CONST_ARGS=""
+CUPLA_CONST_ARGS="${CUPLA_CONST_ARGS} -DCMAKE_BUILD_TYPE=${CUPLA_BUILD_TYPE}"
+CUPLA_CONST_ARGS="${CUPLA_CONST_ARGS} ${CUPLA_CMAKE_ARGS}"
+
+CMAKE_CONFIGS=()
+for CXX_VERSION in $CUPLA_CXX; do
+    for BOOST_VERSION in ${CUPLA_BOOST_VERSIONS}; do
+	for ACC in ${ALPAKA_ACCS}; do
+	    CMAKE_CONFIGS+=("${CUPLA_CONST_ARGS} -DCMAKE_CXX_COMPILER=${CXX_VERSION} -DBOOST_ROOT=/opt/boost/${BOOST_VERSION} -D${ACC}=ON")
+	done
+    done
+done
+
+###################################################
+# build an run tests
+###################################################
 
 # use one build directory for all build configurations
 mkdir build
 cd build
-echo "Build type-> $CUPLA_BUILD_TYPE"
+
+export cupla_DIR=$CI_PROJECT_DIR
 
 # ALPAKA_ACCS contains the backends, which are used for each build
 # the backends are set in the sepcialized base jobs .base_gcc,.base_clang and.base_cuda
-for CMAKE_FLAGS in $ALPAKA_ACCS ; do
+for CONFIG in $(seq 0 $((${#CMAKE_CONFIGS[*]} - 1))); do
+    CMAKE_ARGS=${CMAKE_CONFIGS[$CONFIG]}
+    echo -e "\033[0;32m///////////////////////////////////////////////////"
+    echo "number of processor threads -> $(nproc)"
+    cmake --version | head -n 1
+    echo "CMAKE_ARGS -> ${CMAKE_ARGS}"
+    echo -e "/////////////////////////////////////////////////// \033[0m \n\n"
+
     echo "###################################################"
     echo "# Example Matrix Multiplication (adapted original)"
     echo "###################################################"
     echo "can not run with CPU_B_SEQ_T_SEQ due to missing elements layer in original SDK example"
     echo "CPU_B_SEQ_T_OMP2/THREADS too many threads necessary (256)"
-    if [[ $CMAKE_FLAGS =~ -*DALPAKA_ACC_GPU_CUDA_ENABLE=ON.* ]]; then
+    if [[ $CMAKE_ARGS =~ -*DALPAKA_ACC_GPU_CUDA_ENABLE=ON.* ]]; then
         cmake $cupla_DIR/example/CUDASamples/matrixMul/ \
-	      -DBOOST_ROOT=/opt/boost/$BOOST_VERSION \
-	      $CMAKE_FLAGS \
-	      -DCMAKE_BUILD_TYPE=$CUPLA_BUILD_TYPE
+	      $CMAKE_ARGS
         make -j
         time ./matrixMul -wA=64 -wB=64 -hA=64 -hB=64
         rm -r * ;
@@ -42,11 +63,9 @@ for CMAKE_FLAGS in $ALPAKA_ACCS ; do
     echo "###################################################"
     echo "can not run with CPU_B_SEQ_T_SEQ due to missing elements layer in original SDK example"
     echo "CPU_B_SEQ_T_OMP2/THREADS too many threads necessary (512)"
-    if [[ $CMAKE_FLAGS =~ -*DALPAKA_ACC_GPU_CUDA_ENABLE=ON.* ]]; then
+    if [[ $CMAKE_ARGS =~ -*DALPAKA_ACC_GPU_CUDA_ENABLE=ON.* ]]; then
         cmake $cupla_DIR/example/CUDASamples/asyncAPI/ \
-	      -DBOOST_ROOT=/opt/boost/$BOOST_VERSION \
-	      $CMAKE_FLAGS \
-	      -DCMAKE_BUILD_TYPE=$CUPLA_BUILD_TYPE
+	      $CMAKE_ARGS
         make -j
         time ./asyncAPI
         rm -r * ;
@@ -56,9 +75,7 @@ for CMAKE_FLAGS in $ALPAKA_ACCS ; do
     echo "# Example Async API (added elements layer)"
     echo "###################################################"
     cmake $cupla_DIR/example/CUDASamples/asyncAPI_tuned/ \
-	  -DBOOST_ROOT=/opt/boost/$BOOST_VERSION \
-	  $CMAKE_FLAGS \
-	  -DCMAKE_BUILD_TYPE=$CUPLA_BUILD_TYPE
+	  $CMAKE_ARGS
     make -j
     time ./asyncAPI_tuned
     rm -r *
@@ -67,9 +84,7 @@ for CMAKE_FLAGS in $ALPAKA_ACCS ; do
     echo "Example vectorAdd (added elements layer)"
     echo "###################################################"
     cmake $cupla_DIR/example/CUDASamples/vectorAdd/ \
-	  -DBOOST_ROOT=/opt/boost/$BOOST_VERSION \
-	  $CMAKE_FLAGS \
-	  -DCMAKE_BUILD_TYPE=$CUPLA_BUILD_TYPE
+	  $CMAKE_ARGS
     make -j
     time ./vectorAdd 100000
     rm -r * ;
