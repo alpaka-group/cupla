5ee78f46953bd87b6628f9c942d709a0541c2446
René Widera <r.widera@hzdr.de>
2023-07-28 10:38:21 +0200

    install clang before the test and deinstall after
    Different clang version with OpenMP can not be insalled side by side
    together therefore it is required to install the compiler before the
    test and deinstall after.

--- script/install_clang.sh
+++ script/install_clang.sh
@@ -3,21 +3,15 @@
 set -e
 set -o pipefail
 
-# merge the PR to the latest version of the destination branch
+# install all required clang version including OpenMP
+clang_version=$(echo $CXX_COMPILER | tr -d "clang++-")
+echo "Clang-version: $clang_version"
 
-cd $CI_PROJECT_DIR
-
-# install all required clang versions
-for CXX_COMPILER in $CUPLA_CXX; do
-    clang_version=$(echo $CXX_COMPILER | tr -d "clang++-")
-    echo "Clang-version: $clang_version"
-
-    if ! agc-manager -e clang@${clang_version}; then
-        apt update
-        apt install -y clang-${clang_version}
-        apt install -y libomp-${clang_version}-dev
-    else
-        CLANG_BASE_PATH="$(agc-manager -b clang@${clang_version})"
-        export PATH=$CLANG_BASE_PATH/bin:$PATH
-    fi
-done
+if ! agc-manager -e clang@${clang_version}; then
+    apt update
+    apt install -y clang-${clang_version}
+    apt install -y libomp-${clang_version}-dev
+else
+    CLANG_BASE_PATH="$(agc-manager -b clang@${clang_version})"
+    export PATH=$CLANG_BASE_PATH/bin:$PATH
+fi
